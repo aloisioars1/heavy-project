27d476be3299cbe58edf8711054c1ae6fd198f5d
aloisioars1 <88538737+aloisioars1@users.noreply.github.com>
2026-08-06 23:47:11 -0300

    Heavy Studio Build: Update Sources/ContentView.swift

--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -9,13 +9,13 @@ struct Message: Identifiable {
 struct ContentView: View {
     @State private var messageText: String = ""
     @State private var messages: [Message] = [
-        Message(text: "Olá! Bem-vindo ao Chiste Grego.", isUser: false)
+        Message(text: "Olá! Bem-vindo ao Heavy Pro App.", isUser: false)
     ]
     
     var body: some View {
         VStack {
             HStack {
-                Text("Chiste Grego")
+                Text("Heavy Pro App")
                     .font(.largeTitle)
                     .fontWeight(.bold)
                 Spacer()
