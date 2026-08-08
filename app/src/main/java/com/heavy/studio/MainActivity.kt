8d8944fe6bdf4d7472e429f48c665c6199479031
aloisioars1 <88538737+aloisioars1@users.noreply.github.com>
2026-08-08 00:53:58 -0300

    Heavy Studio Build: Update app/src/main/java/com/heavy/studio/MainActivity.kt

--- app/src/main/java/com/heavy/studio/MainActivity.kt
+++ app/src/main/java/com/heavy/studio/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : AppCompatActivity() {
     private val isDarkTheme: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
